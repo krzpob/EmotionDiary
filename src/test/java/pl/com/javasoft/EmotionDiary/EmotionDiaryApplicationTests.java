0e643c3f849e070ece3f9205e5b30ddcbe5b39df
Krzysztof Pobożan <krzpob@gmail.com>
2023-08-29 23:27:20 +0200

    add some base code

---
+++ src/test/java/pl/com/javasoft/EmotionDiary/EmotionDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.javasoft.EmotionDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
